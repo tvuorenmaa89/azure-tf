a7662a689f02f0cdc607af8b730e9ae5234bf6de
Tobias Vuorenmaa <tobias.vuorenmaa@azr-lab.net>
2020-11-02 10:56:55 +0100

    Initial pull

---
+++ maint.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-azure-tf"
+    storage_account_name = "stgazuretf"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "rg-hello-azure" {
+  name     = "rg-hello-azure"
+  location = "westeurope"
+}
